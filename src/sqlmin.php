8970caa2ef3767109aacfdfb62b7037159f79bf8
Anton Medvedev <anton@medv.io>
2025-01-04 10:06:42 +0100

    wip

--- src/sqlmin.php
+++ src/sqlmin.php
@@ -1,24 +1,32 @@
 <?php
 
-$database = __DIR__ . '/../chinook.sqlite';
+$databases = [
+    __DIR__ . '/../chinook.sqlite',
+    __DIR__ . '/../northwind.db',
+];
 
 // Next goes the actual code. Do not modify anything below this line.
 // ==================================================================
 
-$response = array();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $response = [];
     $request = json_decode(file_get_contents('php://input'), true);
-    $query = $request['query'];
-    $db = new PDO('sqlite:' . $database);
-    $result = $db->query($query);
-    $response['rows'] = $result->fetchAll(PDO::FETCH_ASSOC);
-    die(json_encode($response, JSON_PRETTY_PRINT));
-}
 
+    switch ($request['type']) {
+        case 'execute':
+            $query = $request['query'];
+            $db = new PDO('sqlite:' . $databases[$request['database']]);
+            $result = $db->query($query);
+            $response['rows'] = $result->fetchAll(PDO::FETCH_ASSOC);
+            break;
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $request = json_decode(file_get_contents('php://input'), true);
+        case 'list':
+            $response['databases'] = array_map(function ($db) {
+                return basename($db);
+            }, $databases);
+            break;
+    }
 
     die(json_encode($response, JSON_PRETTY_PRINT));
 }
