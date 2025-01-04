eeda269155dec3db514450b465cb375770e77594
Anton Medvedev <anton@medv.io>
2025-01-04 16:06:07 +0100

    wip

--- src/sqlmin.php
+++ src/sqlmin.php
@@ -13,19 +13,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $response = [];
     $request = json_decode(file_get_contents('php://input'), true);
 
-    switch ($request['type']) {
-        case 'execute':
-            $query = $request['query'];
-            $db = new PDO('sqlite:' . $databases[$request['database']]);
-            $result = $db->query($query);
-            $response['rows'] = $result->fetchAll(PDO::FETCH_ASSOC);
-            break;
+    try {
+        switch ($request['type']) {
+            case 'execute':
+                $query = $request['query'];
+                $db = new PDO('sqlite:' . $databases[$request['database']]);
+                $result = $db->query($query);
+                $response['rows'] = $result->fetchAll(PDO::FETCH_ASSOC);
+                break;
 
-        case 'list':
-            $response['databases'] = array_map(function ($db) {
-                return basename($db);
-            }, $databases);
-            break;
+            case 'list':
+                $response['databases'] = array_map(function ($db) {
+                    return basename($db);
+                }, $databases);
+                break;
+        }
+    } catch (PDOException $e) {
+        $response['error'] = $e->getMessage();
     }
 
     die(json_encode($response, JSON_PRETTY_PRINT));
